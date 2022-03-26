44e9158d2647aafe415cb2d095b8fe8b12e38cf5
Maya <mayagozel5577@gmail.com>
2022-03-26 11:54:39 -0700

    Conditionals and Loops - The else if statement

---
+++ Else if.js
@@ -0,0 +1,28 @@
+/*
+Тhe result of an exam will be determined as follows։
+If the score is
+88 and above => excellent
+40-87 => good
+0-39 => fail
+You are given a program that takes the score as input.
+Task
+Complete the code to output the corresponding result (excellent, good, fail) to the console.
+Sample Input
+78
+Sample Output
+good
+Use console.log() to output the result to the console.
+Use logical operator && to chain multiple conditions.
+*/
+
+var score = parseInt(readLine(), 10)
+    /*
+    88 and above => excellent
+    40-87   => good
+    0-39 => fail
+    */
+    // your code goes here
+
+if (score >= 88) console.log("excellent");
+else if (score >= 40 && score < 88) console.log("good");
+else console.log("fail");
