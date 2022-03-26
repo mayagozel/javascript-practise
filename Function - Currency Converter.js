8c14e493247dc636d063545b16d672a8f179b6c1
Maya <mayagozel5577@gmail.com>
2022-03-26 12:46:17 -0700

    Functions - Final project - Currency Converter

---
+++ Function - Currency Converter.js
@@ -0,0 +1,23 @@
+/*
+You are making a currency converter app.
+Create a function called convert, which takes two parameters: the amount to convert, and the rate, and returns the resulting amount.
+The code to take the parameters as input and call the function is already present in the Playground.
+Create the function to make the code work.
+Sample Input:
+100
+1.1
+Sample Output:
+110
+Converting 100 at the rate of 1.1 is equal to 100*1.1 = 110.
+*/
+
+function main() {
+    var amount = parseFloat(readLine(), 10);
+    var rate = parseFloat(readLine(), 10);
+
+    console.log(convert(amount, rate));
+}
+
+function convert(amount, rate) {
+    return amount * rate;
+}
