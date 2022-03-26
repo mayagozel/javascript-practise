989aef056ef2c8307df50789b50e81b529eded13
Maya <mayagozel5577@gmail.com>
2022-03-26 13:26:28 -0700

    Objects - Adding methods

---
+++ Adding Methods2.js
@@ -0,0 +1,36 @@
+/*
+The program you are given should calculate the annual income of a given loan.
+The program takes the loan initial amount and the annual interest percentage as input, then creates the loan object.
+Complete the function outside the constructor to calculate the annual income, then assign that value to the corresponding field, that you also create, of the constructor in order to execute the given output.
+Sample Input
+15000
+20
+Sample Output
+3000
+Explanation
+The output represent the yearly income of the loan with a 15000 initial amount and 20% annual interest : 20/100*15000 = 3000.
+You should create a new property in the constructor to store the annual income value returned from the calculating function.
+*/
+
+function main() {
+    //get the initial amount and the interest percentage
+    var amount = parseInt(readLine(), 10);
+    var yearPercent = parseInt(readLine(), 10);
+
+    var loan1 = new Loan(amount, yearPercent);
+
+    //output to console
+    console.log(loan1.yearIncome());
+}
+
+function Loan(amount, percent) {
+    this.amount = amount;
+    this.yearPercent = percent;
+    //your code goes here
+    this.yearIncome = calcYearIncome;
+};
+
+function calcYearIncome() {
+    //complete the function to calculate yearly income
+    return this.amount * this.yearPercent / 100;
+}
