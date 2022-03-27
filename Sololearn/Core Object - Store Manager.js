e47f04206e7418ebf7410b693f08b05da76c5195
Maya <mayagozel5577@gmail.com>
2022-03-27 14:31:15 -0700

    Core object - Final project - Store Manager

---
+++ Sololearn/Core Object - Store Manager.js
@@ -0,0 +1,18 @@
+/*
+You are working on a Store Manager program, which stores the prices in an array.
+You need to add functionality to increase the prices by the given amount.
+The increase variable is taken from user input. You need to increase all the prices in the given array by that amount and output to the console the resulting array.
+Use a loop to iterate through the array and increase all items.
+*/
+
+function main() {
+    var increase = parseInt(readLine(), 10);
+    var prices = [98.99, 15.2, 20, 1026];
+    //your code goes here
+    for (i = 0; i < prices.length; i++) {
+        var result = prices[i] + increase;
+        prices[i] = result;
+
+    }
+    console.log(prices);
+}
