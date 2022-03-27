5047626ae92c967437d6a29a4add06a72472d30d
Maya <mayagozel5577@gmail.com>
2022-03-27 10:59:45 -0700

    Arrays - Fluffy pancakes

---
+++ Sololearn/Arrays.js
@@ -0,0 +1,27 @@
+/*
+The array you are given represents the menu of breakfast options available at the hotel.
+The Chef decided to replace one of the options with "Fluffy Pancakes".
+Write a program to take the index as input, replace the element with that index with "Fluffy Pancakes", and output the new menu to the console as an array.
+Sample Input
+2
+Sample Output
+[
+'Cinnamon Doughnuts',
+'Waffles',
+'Fluffy Pancakes',
+'Chorizo Burrito',
+'French Toast'
+]
+The element with index 2 has been replaced in the output array.
+Remember that the first element of an array has 0 index.
+*/
+
+function main() {
+    var breakfasts = ['Cinnamon Doughnuts', 'Waffles', 'Granola', 'Chorizo Burrito', 'French Toast'];
+    var index = parseInt(readLine(), 10)
+
+    //replace the corresponding element by "Fluffy Pancakes"
+    breakfasts[index] = "Fluffy Pancakes";
+    //output the menu to the console
+    console.log(breakfasts);
+}
