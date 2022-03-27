64d9136a9f7cbd7b5104aa94e27d0d923599b01a
Maya <mayagozel5577@gmail.com>
2022-03-27 13:57:14 -0700

    The Date Objects - Monday to Sunday

---
+++ Sololearn/The Date Object.js
@@ -0,0 +1,27 @@
+/*
+The program you are given takes year, month and day as input.
+Create a function that takes them as arguments and returns the corresponding day of the week.
+Sample Input
+1993
+7
+12
+Sample Output
+Thursday
+Hint: The given code creates a Date object from the parameters. Use the getDay() method of the date object to get the index, then use it in the given names array to return the name of the day.
+*/
+
+function main() {
+    var year = parseInt(readLine(), 10);
+    var month = parseInt(readLine(), 10);
+    var day = parseInt(readLine(), 10);
+
+    console.log(getWeekDay(year, month, day));
+}
+
+function getWeekDay(year, month, day) {
+    var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var d = new Date(year, month, day);
+    //complete the function
+    var dayOfTheWeek = d.getDay()
+    return names[dayOfTheWeek];
+}
