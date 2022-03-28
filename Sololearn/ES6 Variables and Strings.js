871503f1bc9ea55eb8caed1c266cc725d387d3c9
Maya <mayagozel5577@gmail.com>
2022-03-28 10:35:49 -0700

    ES6 Variables and Strings - London is the capital of GB

---
+++ Sololearn/ES6 Variables and Strings.js
@@ -0,0 +1,26 @@
+/*
+You need to make country cards for a school project.
+The given program takes the country and its capital name as input.
+Complete the function to return a string in the format you are given in the sample output:
+
+Sample Input
+Portugal
+Lisbon
+
+Sample Output
+Name: Portugal, Capital: Lisbon
+Use template literals to output variables in strings.
+*/
+
+function main() {
+    var country = readLine();
+    var capital = readLine();
+
+    console.log(countryCard(country, capital));
+}
+
+function countryCard(country, capital) {
+    //complete the function
+    //use backtick (` `) for template literal
+    return `Name: ${country}, Capital: ${capital}`;
+}
