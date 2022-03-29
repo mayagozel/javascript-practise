11703505bacc6cd6cdf122e826d2838424cb373f
Maya <mayagozel5577@gmail.com>
2022-03-29 10:34:59 -0700

    Moved some files

---
+++ Sololearn/Functions in ECMAScript 6.js
@@ -0,0 +1,10 @@
+/*
+You have created an array containing the salaries of your factory workers.
+The manager at the factory has decided to give salary raises to his best workers and needs to the see the impact of these increases on the budget.
+The program you are given takes the salary percent increase as input. Complete the given function to use the percent as a parameter, then calculate and return the total salary increase for all of the workers in the array.
+Sample Input
+10
+Sample Output
+3000
+Use the forEach() function to operate with each element of the array.
+*/
